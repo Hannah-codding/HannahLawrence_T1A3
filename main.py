5c7258bf9566a6c5ba2cb4214074b90a109ca982
Hannah <162410909+Hannah-codding@users.noreply.github.com>
2024-05-09 20:27:38 +1000

    Update main.py updated to show a link

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
 # System packages
 import os.path
+import random
 
 
 # Imports of our own functions
 from visual_elements import hang_a, hang_b, hang_c, hang_d, hang_end, hang_start, gameover_message, looser_message, winner_message
+#from hangman_functions import add_hangman, remove_hangman, mark_hangman, view_hangman 
+
 
 # External packages
-from colored import Fore, Back, Style
-
-# Imports of our own functions
-from hangman_functions import add_hangman, remove_hangman, mark_hangman, view_hangman
-
+# from colored import Fore, Back, Style
 
 
 
@@ -19,26 +18,35 @@ from hangman_functions import add_hangman, remove_hangman, mark_hangman, view_ha
 def choose_word():
     word_list = ["plant", "happy", "hello", "whale", "silly"]
     return random.choice(word_list).upper()
-#  list of words that the program will run through
+#  list of words that the program will run. the user will hopefully not see this so that they can guess the answers. 
 
 
-def display_word(word, guessed_letters):
+
+#  Stages of hangman visual, this should be run in order by the script
+hang_stages = [hang_start, hang_a, hang_b, hang_c, hang_d, hang_end]
+
+
+
+#  Letter guessing 
+def display_word(word, guessed_letters): # setting peramitors for the letter guessing 
     display = ""
     for letter in word:
         if letter in guessed_letters:
             display += letter
         else:
-            display += "_"
+            display += "_" 
     return display
 
+
 def main():
-    word = choose_word()
+    word = choose_word() # this will be sourced from the word list on line 20. Set to be random to make the game more difficult
     guessed_letters = set()
-    tries = 6
-    
-    
-    print(f"{Fore.red}{Back.purple} Welcome to the HANGMAN application {Style.reset}")
-    #  alternative script- print(f"{Fore.black}{Back.white}Welcome to the HANGMAN application!\n\n")
+    tries = 6    # amount of tries the player gets before the correct word. this aligns to the hangma graphic.  
+
+
+    # print(f"{Fore.pink}{Back.pink} Welcome to the HANGMAN application {Style.reset}")
+    print("Welcome to the HANGMAN application") # styling applied to the first line
+ #####  alternative script---= print(f"{Fore.black}{Back.white}Welcome to the HANGMAN application!\n\n")
     print(display_word(word, guessed_letters))
 
     while not all(letter in guessed_letters for letter in word) and tries > 0:
@@ -46,20 +54,24 @@ def main():
 
         if len(guess) == 1 and guess.isalpha():
             if guess in guessed_letters:
-                print("You've already guessed that letter.")
+                print("You've already guessed that letter.") # shown when the user has already guessed a letter
             elif guess in word:
-                print("Good guess!")
+                print("Good guess!") 
                 guessed_letters.add(guess)
+                print(hang_stages[6 - tries])
             else:
-                print("Wrong guess!")
+                print("Wrong guess! Good try!")
                 tries -= 1
+                print(hang_stages[6 - tries])
         elif len(guess) == len(word) and guess.isalpha():
             if guess == word:
                 print("Congratulations! You guessed the word!")
-                break
+                print (winner_message)
+                  #break
             else:
-                print("Wrong guess!")
+                print("Wrong guess! Good try!")
                 tries -= 1
+                print(gameover_message)
         else:
             print('Not a valid guess.')
 
@@ -67,14 +79,14 @@ def main():
         print(f"Tries left: {tries}")
 
     if all(letter in guessed_letters for letter in word):
-        print("Congratulations! You guessed the word!")
+        print("You guessed the word!")
+        print(winner_message)
+
     else:
         print(f"Sorry, you lost! The word was: {word}")
-
-if __name__ == "__main__":
-    main()
-
-
+        print (looser_message)
+        
+    print(gameover_message)   #final message at the end of the above script.
 
 
 
@@ -83,56 +95,6 @@ if __name__ == "__main__":
 
 
 
-
-
-
-
-
-
-
-
-
-
-#  Subheading:
-print("Choose the first letter for the hangman!")
-
-def create_menu():
-    user_choice = input       ("Enter your letter: ")
-    return user_choice
-
-
-
-
-while not gessed and tried > 0:
-    guess = input('please guess a letter or wrd: ').upper()
-   
-    if len (guess) == 1 and guess.isalpha():
-    
-    elif len (guess) == len(word) and guess.isalpha()
-   
-    else: 
-        print('Not a valid guess.')
-
-
-#  Letter guessing 
-
-print('WRONG! Try again!')
-
-print('Correct! Excellet!').upper()
-
-print('WINNER!') 
-
-print('YOU LOOSE! Better luck next time!')
-
-
-
-
-
-
-file_name = "list.csv"
-
-
-print("Thank you for using the hangman application")
 
 
 
