15a671ff41110cfae10a7c2eb391e385286b4c59
Hannah <162410909+Hannah-codding@users.noreply.github.com>
2024-05-11 15:02:39 +1000

    Add files via upload

---
+++ Wordlist.py
@@ -0,0 +1,9 @@
+#  Secret word list below 'shhh'
+#  note that i have tried to stick to the pep8 style convention
+
+word_list = ["true"]
+
+words_list = ["happy" "hello" "yellow", "silly", "dog", 
+             "frog", "cat", "world", "ant", "blue", "conscious",
+             "adhesive", "elite","dad", "grade", "past", "coal",
+             "nippy", "hurried", "shocking", "abortive"]
